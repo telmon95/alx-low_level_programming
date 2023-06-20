4cf9d6448e941d00d1fffb11cf55515e3a978d6f
telmon95 <telmonm@gmail.com>
2023-06-20 16:37:15 -0700

    test commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * print_alphabet - creates alphabets in order
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
