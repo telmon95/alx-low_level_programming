7190b7297c3e85bb6e38d7e0db11be2c0be499d8
telmon95 <telmonm@gmail.com>
2023-06-21 10:28:12 -0700

    correctons task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,7 @@ void times_table(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 10; j++)
+	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
@@ -31,6 +31,6 @@ void times_table(void)
 				_putchar((k % 10) + '0');
 			}
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
