b336433ef6df752ec20c8732dde64545b8f571d2
telmon95 <telmonm@gmail.com>
2023-07-13 15:09:48 -0700

    last commit for task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,28 +4,11 @@
 
 #define ERR_MSG "Error"
 
-int is_digit(char *s);
-int _strlen(char *s);
-void errors(void);
-int multiply(char *s1, char *s2);
-
 /**
- * _putchar - Writes a character to the standard output.
- * @c: The character to be written.
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
  *
- * Return: On success, the character written is returned.
- *         On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return putchar(c);
-}
-
-/**
- * is_digit - checks if a string contains a non-digit char.
- * @s: string to be evaluated.
- *
- * Return: 0 if a non-digit is found, 1 otherwise.
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
 int is_digit(char *s)
 {
@@ -41,105 +24,78 @@ int is_digit(char *s)
 }
 
 /**
- * _strlen - returns the length of a string.
- * @s: string to evaluate.
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- * Return: the length of the string.
+ * Return: the length of the string
  */
 int _strlen(char *s)
 {
 	int i = 0;
 
 	while (s[i] != '\0')
+	{
 		i++;
-
+	}
 	return (i);
 }
 
 /**
- * errors - handles errors for main.
+ * errors - handles errors for main
  */
 void errors(void)
 {
-	int i;
-
-	for (i = 0; ERR_MSG[i]; i++)
-		_putchar(ERR_MSG[i]);
-	_putchar('\n');
+	printf("Error\n");
 	exit(98);
 }
 
 /**
- * main - multiplies two positive numbers.
- * @argc: number of arguments.
- * @argv: array of arguments.
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: always 0 (Success).
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	s1 = argv[1];
-	s2 = argv[2];
-
+	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
-
-	int result_len = _strlen(s1) + _strlen(s2) + 1;
-	int *result = malloc(sizeof(int) * result_len);
-
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-
-	for (int i = 0; i < result_len; i++)
+	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-
-	multiply(s1, s2);
-
-	int a = 0;
-	for (int i = 0; i < result_len - 1; i++)
-	{
-		if (result[i])
-			a = 1;
-
-		if (a)
-			_putchar(result[i] + '0');
-	}
-
-	if (!a)
-		_putchar('0');
-
-	_putchar('\n');
-	free(result);
-	return (0);
-}
-
-/**
- * multiply - performs the multiplication of two numbers.
- * @s1: first number.
- * @s2: second number.
- */
-int multiply(char *s1, char *s2)
-{
-	int len1 = _strlen(s1);
-	int len2 = _strlen(s2);
-	int carry, digit1, digit2;
-
-	for (int len1 = len1 - 1; len1 >= 0; len1--)
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
-
-		for (int len2 = len2 - 1; len2 >= 0; len2--)
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
-
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
+	_putchar('\n');
+	free(result);
+	return (0);
 }
