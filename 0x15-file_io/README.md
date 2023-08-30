bb8bb1396a50c14ac288ae9d32f8aff3b24257ee
telmon95 <Telmonm@gmail.com>
2023-08-30 09:37:35 +0200

    Delete 0x15-file_io directory

--- 0x15-file_io/README.md
+++
@@ -1,1 +0,0 @@
-Tasks for 0x15. C - File I/O
--- 0x15-file_io/elf_printers.h
+++
@@ -1,8 +0,0 @@
-#ifndef ELF_PRINTERS_H
-#define ELF_PRINTERS_H
-
-#include <elf.h>
-
-void print_type(unsigned int e_type, unsigned char *e_ident);
-
-#endif
