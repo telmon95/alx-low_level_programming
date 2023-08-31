caf6d3892658c2b111211f2588f3bbcc1e0f3d45
telmon95 <telmonm@gmail.com>
2023-08-31 15:44:03 -0700

    re-commit ii

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,7 +7,7 @@
  *
  * Return: 1 for success, -1 for failure
  */
-int bit_set(unsigned long int *n, unsigned int index)
+int set_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
