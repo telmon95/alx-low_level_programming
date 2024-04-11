88edc011cacaf0a465c5e89a2e33af00b272ce2e
telmon95 <telmonm@gmail.com>
2024-04-10 18:17:34 -0700

    another correction

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,37 +1,28 @@
-#include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * linear_search - searches for a value in an array
- * 		   of integers using linear search
- * @array: A pointer to the first element of the array to search.
- * @size: The number of elements in the array.
- * @value: the value to search for.
+ * linear_search - searches for a value in an array using linear search algorithm
+ * @array: pointer to the first element of the array to search
+ * @size: number of elements in the array
+ * @value: value to search for
  *
- * Return: if the value is not present or the array is NULL, -1,
- * 	   otherwise, the first index where it is located.
+ * Return: index of the first occurrence of value in array, or -1 if not found
  */
-
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+    size_t i; /* Variable to iterate over the array */
 
-	/* check if the array is NULL */
-	if (array == NULL)
-		return (-1);
+    if (array == NULL) /* Check if array is NULL */
+        return (-1); /* If so, return -1 as value not found */
 
-	/* iterate over each element of the array */
-	for (i = 0; i < size; i++)
-	{
-		/* print the value being checked */
-		printf("value checked[%1lu] = [%d]\n", (unsigned long)i, array[i]);
+    for (i = 0; i < size; i++) /* Iterate over each element of the array */
+    {
+        printf("Value checked array[%lu] = [%d]\n", i, array[i]); /* Print the value being checked */
 
-	/* if the value is found */
-	if (array[i] == value)
+        if (array[i] == value) /* If the value is found */
+            return (i); /* Return the index of the value */
+    }
 
-		/*return the index of the value */
-		return (i);
-	}
-
-	/* value not found, return -1 */
-	return (-1);
+    return (-1); /* Value not found, return -1 */
 }
